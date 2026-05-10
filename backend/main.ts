1e5ab04b10fad712f988e21ea52727154e69614a
Krixsick <linusgao123@gmail.com>
2026-05-10 14:02:23 -0400

    Working on food services from waterloo api

--- backend/main.ts
+++ backend/main.ts
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 import libraryRouter from "./apis/library";
 import axios from "axios";
+import "dotenv/config";
+import { configDotenv } from "dotenv";
 const app = express();
-
+configDotenv();
 app.use("/library/hours", libraryRouter);
 app.use(cors());
 
