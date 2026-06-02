62c63817e773a87c31d0da8e8782c3cf7b091327
Jolene Huang <jolenehuang512@gmail.com>
2026-06-02 19:22:07 -0400

    organization

--- waterloo-map/src/App.tsx
+++ waterloo-map/src/App.tsx
@@ -1,46 +1,7 @@
-import { useEffect, useRef } from "react";
-import mapboxgl from "mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
-import "./App.css";
-
-mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
+import Map from "./components/Map";
 
 function App() {
-  const mapContainer = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    if (!mapContainer.current) return;
-  
-    const map = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/navigation-night-v1",
-      center: [-80.5449, 43.4723],
-      zoom: 16,
-      pitch: 65,
-      bearing: -25,
-    });
-  
-    map.on("load", () => {
-      map.addLayer({
-        id: "3d-buildings",
-        source: "composite",
-        "source-layer": "building",
-        filter: ["==", "extrude", "true"],
-        type: "fill-extrusion",
-        minzoom: 15,
-        paint: {
-          "fill-extrusion-color": "#aaa",
-          "fill-extrusion-height": ["get", "height"],
-          "fill-extrusion-base": ["get", "min_height"],
-          "fill-extrusion-opacity": 0.6,
-        },
-      });
-    });
-  
-    return () => map.remove();
-  }, []);
-
-  return <div className="map-container" ref={mapContainer}></div>;
+  return <Map />;
 }
 
 export default App;
