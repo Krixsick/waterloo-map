89e262c67afc359a0fe7893a3e7cf8ee7f6c3fd6
Krixsick <linusgao123@gmail.com>
2026-06-09 12:41:07 -0400

    added tailwindcss and tanstack query

--- waterloo-map/src/App.tsx
+++ waterloo-map/src/App.tsx
@@ -1,7 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Map from "./components/Map";
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryClient = new QueryClient();
 function App() {
-  return <Map />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {/* The rest of your application */}
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Map></Map>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
