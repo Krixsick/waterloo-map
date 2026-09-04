94dd1b6a93f6a030d1ea03a80e0908a9371c105f
Jolene Huang <jolenehuang512@gmail.com>
2026-09-04 10:59:53 -0400

    scraped food hours and info

--- backend/main.ts
+++ backend/main.ts
@@ -5,6 +5,8 @@ import gymRouter from "./apis/gym";
 import campus_food_router from "./apis/food/campusFood";
 import eventRouter from "./apis/event";
 import transitRouter from "./apis/transit";
+import campusFoodRouter from "./apis/food/campusFood";
+import residenceFoodRouter from "./apis/food/residenceFood";
 
 import "dotenv/config";
 import { configDotenv } from "dotenv";
@@ -35,5 +37,7 @@ app.use("/gym", gymRouter);
 app.use("/food/campus", campus_food_router);
 app.use("/events", eventRouter);
 app.use("/transit", transitRouter);
+app.use("/food/campus", campusFoodRouter);
+app.use("/food/residence", residenceFoodRouter);
 
 app.listen(3001, () => console.log("listening on :3001"));
--- backend/types/food.ts
+++ backend/types/food.ts
@@ -1,7 +1,7 @@
 export interface CampusFoodInfo {
   name: string;
   location: string | null;
-  features: string[];
+  description: string;
   paymentMethods: string[];
   hours: Record<string, string>;
   exceptions: string[];
